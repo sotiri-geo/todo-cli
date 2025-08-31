02f49912cf19b18079869cd4550ce40b20c3112b
Sotiri Georgiou <tirigeorgiou@gmail.com>
2025-08-31 08:27:07 +0100

    test: add task autoincrements id

--- task/task.go
+++ task/task.go
@@ -9,7 +9,16 @@ type Task struct {
 	CreatedAt   time.Time
 }
 
+type TaskList struct {
+	Tasks []Task
+}
+
 // Have constructors alway return pointers to the structs they create
 func NewTask(description string, id int, createdAt time.Time) *Task {
 	return &Task{ID: id, Description: description, Completed: false, CreatedAt: createdAt}
+}
+
+func (t *TaskList) AddTask(description string) {
+	task := NewTask(description, len(t.Tasks)+1, time.Now())
+	t.Tasks = append(t.Tasks, *task)
 }
--- task/task_test.go
+++ task/task_test.go
@@ -5,9 +5,6 @@ import (
 	"time"
 )
 
-// Start with the domain model, smallest unit of value
-// "I can create a new task, it starts incomplete and has a description"
-
 func TestNewTask(t *testing.T) {
 	t.Run("Creates a new task", func(t *testing.T) {
 		task := NewTask("buy milk", 1, time.Date(2025, 8, 31, 12, 0, 0, 0, time.UTC))
@@ -19,6 +16,26 @@ func TestNewTask(t *testing.T) {
 
 		if task.Completed {
 			t.Error("New task should not be completed")
+		}
+	})
+}
+
+func TestAddToListOfTasks(t *testing.T) {
+	t.Run("auto increments ID", func(t *testing.T) {
+		// User should only configure the min amount they need to i.e. just description
+		// all other stateful variables should be handled by business logic: ID, CreatedAt, Completed (when adding a task)
+		list := TaskList{}
+		list.AddTask("buy milk")
+		list.AddTask("buy bread")
+
+		if len(list.Tasks) != 2 {
+			t.Error("failed to add 2 tasks")
+		}
+		if list.Tasks[0].ID != 1 {
+			t.Errorf("got ID: %d, want ID: %d from task %q", list.Tasks[0].ID, 1, list.Tasks[0].Description)
+		}
+		if list.Tasks[1].ID != 2 {
+			t.Errorf("got ID: %d, want ID: %d from task %q", list.Tasks[1].ID, 2, list.Tasks[1].Description)
 		}
 	})
 }
